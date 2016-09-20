e1788f2508134905520dc624d6b0eba612374216
danbras <daniel@filmeametro.com>
2016-09-19 21:45:14 -0500

    1
    1

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,12 +10,24 @@ const msgDefaults = {
   icon_emoji: config('ICON_EMOJI')
 }
 
-let testvar = Math.floor(Math.random() * (9 -0)) + 0;
-let testvar2 = ['Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10']
+let testvar = Math.floor(Math.random() * (9 -0)) + 0
+let testvar2 = 
+[
+'>It is a capital mistake to theorize before one has data. - _Sherlock Holmes_',
+'Q2',
+'Q3',
+'Q4',
+'Q5',
+'Q6',
+'Q7',
+'Q8',
+'Q9',
+'Q10'
+]
 
 let attachments = [
   {
-    title: testvar+testvar2[testvar]+'TEST Starbot will help you find the hippest repos on GitHub',
+    title: testvar+testvar2[0]+'TEST Starbot will help you find the hippest repos on GitHub',
     color: '#2FA44F',
     text: '`/starbot repos` returns hip repos \n`/starbot javascript` returns hip JavaScript repos',
     mrkdwn_in: ['text']
