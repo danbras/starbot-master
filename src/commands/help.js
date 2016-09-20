417ed42837aed59a5ff77b066bd7095443097d5e
danbras <daniel@filmeametro.com>
2016-09-19 21:39:02 -0500

    5
    5

--- src/commands/help.js
+++ src/commands/help.js
@@ -15,7 +15,7 @@ let testvar2 = ['Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10']
 
 let attachments = [
   {
-    title: testvar+testvar2[testvar]'TEST Starbot will help you find the hippest repos on GitHub',
+    title: testvar+testvar2[testvar]+'TEST Starbot will help you find the hippest repos on GitHub',
     color: '#2FA44F',
     text: '`/starbot repos` returns hip repos \n`/starbot javascript` returns hip JavaScript repos',
     mrkdwn_in: ['text']
