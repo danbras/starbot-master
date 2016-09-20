904d3d4e170932b657136891344db3bd87a396d4
danbras <daniel@filmeametro.com>
2016-09-19 21:37:46 -0500

    4
    4

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,11 +10,12 @@ const msgDefaults = {
   icon_emoji: config('ICON_EMOJI')
 }
 
-let testvar = Math.floor(Math.random() * (10 - 1)) + 1;
+let testvar = Math.floor(Math.random() * (9 -0)) + 0;
+let testvar2 = ['Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10']
 
 let attachments = [
   {
-    title: testvar+'TEST Starbot will help you find the hippest repos on GitHub',
+    title: testvar+testvar2[testvar]'TEST Starbot will help you find the hippest repos on GitHub',
     color: '#2FA44F',
     text: '`/starbot repos` returns hip repos \n`/starbot javascript` returns hip JavaScript repos',
     mrkdwn_in: ['text']
